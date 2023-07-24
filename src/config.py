e93fe770e179d55ed3a3dbf9c38761e3383d11bb
DipodDP <dipoddp@gmail.com>
2023-07-25 00:08:21 +0700

    Try to delete pydantic-settings from config

--- src/config.py
+++ src/config.py
@@ -1,25 +1,34 @@
 # global configs
 import os
 
-from pydantic import Field
+# from pydantic import Field
 # from typing import Any, Callable, Set
 # from pydantic import (AliasChoices, AmqpDsn, BaseModel, Field, ImportString,
 #                       PostgresDsn, RedisDsn)
-from pydantic_settings import BaseSettings, SettingsConfigDict
+# from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '../.env'))
 
 
-class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore", )
-    auth_key: str = Field(default="Secret", validation_alias="my_auth_key")
-    db_url: str = Field(
-        default="sqlite+aiosqlite:///" + os.path.join(basedir, "app.db")
-    )
-    test_db_url: str = ""
-    invest_token: str
-    sandbox_invest_token: str
-
+# class Settings(BaseSettings):
+class Settings():
+    # model_config = SettingsConfigDict(env_file=".env", extra="ignore", )
+    # auth_key: str = Field(default="Secret", validation_alias="my_auth_key")
+    # db_url: str = Field(
+    #     default="sqlite+aiosqlite:///" + os.path.join(basedir, "app.db")
+    # )
+    # test_db_url: str = ""
+    # invest_token: str
+    # sandbox_invest_token: str
+    auth_key: str = os.environ.get('AUTH_KEY') or '\xbf\xb0\x11\xb1\xcd\xf9\xba\x8bp\x0c...'
+    db_url: str = os.environ.get('DB_URL') or "sqlite+aiosqlite:///" + os.path.join(basedir, "app.db")
+    test_db_url: str = os.environ.get('TEST_DB_URL') or "sqlite+aiosqlite:///" + os.path.join(basedir, "test_app.db")
+    invest_token = os.environ.get('INVEST_TOKEN')
+    sandbox_invest_token = os.environ.get('SANDBOX_INVEST_TOKEN')
+    #
 
     # redis_dsn: RedisDsn = Field(
     #     "redis://user:pass@localhost:6379/1",
@@ -39,4 +48,4 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
-# print(settings.model_dump())
+# print(settings.auth_key)
