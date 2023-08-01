277ef89a72978073d297e086908860b55044f646
DipodDP <>
2023-08-01 10:46:21 +0300

    feat(tk_api): added instruments cache

--- src/tk_api/service/instruments.py
+++ src/tk_api/service/instruments.py
@@ -1,6 +1,8 @@
-from typing import List
+from typing import Any, List
 from pandas import DataFrame
-from tinkoff.invest import InstrumentIdType, Share
+from tinkoff.invest import Client, InstrumentIdType, Share
+from tinkoff.invest.caching.instruments_cache.instruments_cache import InstrumentsCache
+from tinkoff.invest.caching.instruments_cache.settings import InstrumentsCacheSettings
 from src.tk_api.schemas import ShareSchema
 from src.tk_api.service.client import TinkoffClientService
 
@@ -11,25 +13,37 @@ class InstrumentsService(TinkoffClientService):
     Takes responsibility for choosing correct function to call basing on
     sandbox mode flag.
     """
+    instruments_cache: InstrumentsCache | None = None
 
-    # def __init__(self, token: str, sandbox: bool = True):
-    #     super().__init__(token, sandbox)
-    #     self.instruments: List[Share]
+    def _get_instruments_cache(self):
+        """Function to get instruments cache"""
+        with Client(self.token) as client:
+            settings = InstrumentsCacheSettings()
+            type(self).instruments_cache = InstrumentsCache(
+                settings=settings, instruments_service=client.instruments
+            )
 
-    async def _get_instruments(self) -> List[Share]:
-        response = await self.servicies.instruments.shares()
-        # todo: cache needed
-        return response.instruments
+    def _get_instruments(self) -> List[Share] | None:
+        """Function to get instruments from cache"""
+        if not self.instruments_cache:
+            print(self.instruments_cache)
+            self._get_instruments_cache()
+        if self.instruments_cache:
+            response = self.instruments_cache.shares()
+            return response.instruments
+        else:
+            return None
 
     async def get_instrument_by_ticker(self, ticker: "str"):
-        shares = await self._get_instruments()
-        
+        """Function to get instrument by ticker"""
+        shares = self._get_instruments()
+        # todo: maybe it should cache dataframe somehow
         df = DataFrame(shares)
         df = df[df['ticker'] == ticker.upper()]
         if df.empty:
             return None
         else:
-            return df.to_dict()
+            # return df.to_dict()
             return await self.get_share(str(df['figi'].iloc[0]))
 
     async def get_share(self, figi: str):
@@ -37,17 +51,18 @@ class InstrumentsService(TinkoffClientService):
                 id_type=InstrumentIdType.INSTRUMENT_ID_TYPE_FIGI,
                 id=figi
         )
-        share = response.instrument
-
-        return ShareSchema(
-            ticker=share.ticker,
-            lot=share.lot,
-            short_enabled_flag=share.short_enabled_flag,
-            otc_flag=share.otc_flag,
-            buy_available_flag=share.buy_available_flag,
-            sell_available_flag=share.sell_available_flag,
-            api_trade_available_flag=share.api_trade_available_flag
-        )
+        return response
+        # share = response.instrument
+        #
+        # return ShareSchema(
+        #     ticker=share.ticker,
+        #     lot=share.lot,
+        #     short_enabled_flag=share.short_enabled_flag,
+        #     otc_flag=share.otc_flag,
+        #     buy_available_flag=share.buy_available_flag,
+        #     sell_available_flag=share.sell_available_flag,
+        #     api_trade_available_flag=share.api_trade_available_flag
+        # )
 
 
 # InstrumentService().servicies.instruments
